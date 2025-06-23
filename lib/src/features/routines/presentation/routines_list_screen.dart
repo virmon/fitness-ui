e6f2118beb2c6b330d7892565f77e23db7b125f6
virmon <virmontumulak@gmail.com>
2025-06-23 20:51:38 +0800

    added base layout for create routine form and exerciseSet form

--- lib/src/features/routines/presentation/routines_list_screen.dart
+++ lib/src/features/routines/presentation/routines_list_screen.dart
@@ -1,5 +1,7 @@
+import 'package:fitness_ui/src/common/typography.dart';
 import 'package:fitness_ui/src/features/routines/presentation/routines_list.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class RoutinesListScreen extends StatelessWidget {
   const RoutinesListScreen({super.key});
@@ -26,9 +28,66 @@ class RoutinesListScreen extends StatelessWidget {
         ]),
         floatingActionButton: FloatingActionButton(
           onPressed: () => {
-            // todo: open create new routine screen
+            showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                  return RoutineCreateForm();
+                })
           },
           child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+class RoutineCreateForm extends StatelessWidget {
+  const RoutineCreateForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 20,
+                bottom: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(onTap: () => context.pop(), child: Icon(Icons.close))
+                ],
+              ),
+            ),
+            const TitleHeader(text: 'Name your new Routine'),
+            SizedBox(
+              width: 350,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800]),
+                    hintText: "My workout plan",
+                    fillColor: Colors.white10,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  // todo: create new routine
+                  // todo: navigate to newly created routine
+                },
+                child: const Text('Create'))
+          ],
         ),
       ),
     );
